21906d7fcbb51710f9b0d722bcb95247c6242d34
Run396 <Run396@users.noreply.github.com>
2023-10-02 13:04:46 +0200

    ui

--- ui/cleanse_data.py
+++ ui/cleanse_data.py
@@ -6,4 +6,6 @@ n = 150000
 df = pd.read_csv('AIS_2023_01_01_1.csv', nrows=n)
 df = df.sort_values(["MMSI","BaseDateTime"])
 
+count = df.groupby(['MMSI']).filter(lambda x: len(x) > 24)
+
 df.to_csv('boats.csv', index=False)
--- ui/map.py
+++ ui/map.py
@@ -3,9 +3,7 @@ import pandas as pd
 
 df = pd.read_csv("boats.csv")
 
-id = df[df['MMSI'] == 367605150]
-
-fig = px.scatter_geo(id,lat='LAT',lon='LON', hover_name="BaseDateTime",color_discrete_sequence=["red"])
+fig = px.scatter_geo(df,lat='LAT',lon='LON', hover_name="BaseDateTime",color_discrete_sequence=["red"])
 fig.update_geos(
     scope="north america",
     resolution = 110, # Set to 50 or 110
