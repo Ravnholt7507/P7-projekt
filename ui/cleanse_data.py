057c5ec6f619d7df39e2cce8bb32878303376d10
Nathan Reece <nreece20@student.aau.dk>
2023-10-04 11:38:11 +0200

    Update cleanse_data.py

--- ui/cleanse_data.py
+++ ui/cleanse_data.py
@@ -3,10 +3,14 @@ pd.set_option('display.max_columns', 20)
 
 n = 150000
 
-df = pd.read_csv('AIS_2023_01_01_1.csv', nrows=n)
+df = pd.read_csv('AIS_2023_01_01.csv', nrows=n)
 df = df.sort_values(["MMSI","BaseDateTime"])
 
+<<<<<<< Updated upstream
 # df = df.groupby(['MMSI']).filter(lambda x: len(x) > 24)
 df = df[df['MMSI'] == 224989000]
+=======
+df = df.groupby(['MMSI']).filter(lambda x: len(x) > 24)
+>>>>>>> Stashed changes
 
 df.to_csv('boats.csv', index=False)
