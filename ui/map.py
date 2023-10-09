ede1dd96c838b40a4eb03a21e0b7ad59e55b779f
Run396 <Run396@users.noreply.github.com>
2023-10-09 10:07:37 +0200

    update igen

--- ui/map.py
+++ ui/map.py
@@ -1,31 +1,21 @@
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv("boats.csv")
+def plot():
+    df = pd.read_csv("boats.csv")
 
-fig = px.scatter_geo(df, lat='LAT', lon='LON', hover_data=['BaseDateTime','COG','Heading'],
-                     hover_name="MMSI", color_discrete_sequence=["red"])
-fig.update_geos(
-    scope="north america",
-    resolution=110,  # Set to 50 or 110
-    showlakes=True, lakecolor="Blue",
-    showrivers=True, rivercolor="Blue"
-)
+    fig = px.scatter_geo(df, lat='LAT', lon='LON', hover_data=['BaseDateTime','COG','Heading'],
+                        hover_name="MMSI", color_discrete_sequence=["red"])
+    fig.update_geos(
+        scope="north america",
+        resolution=110,  # Set to 50 or 110
+        showlakes=True, lakecolor="Blue",
+        showrivers=True, rivercolor="Blue"
+    )
 
-# plot only two coordinates for testing in green color
-#COG
-LAT = 24.49526005
-LON = -79.86377995
+    df2 = pd.read_csv("predictions.csv")
 
-#Heading
-LAT2 = 24.49891825
-LON2 = -79.86012175
+    fig.add_trace(px.scatter_geo(df2,lat='LAT', lon='LON',hover_name="MMSI", color_discrete_sequence=["green"]).data[0])
 
-fig.add_trace(px.scatter_geo(lat=[LAT], lon=[
-              LON], color_discrete_sequence=["green"]).data[0])
-
-fig.add_trace(px.scatter_geo(lat=[LAT2], lon=[
-              LON2], color_discrete_sequence=["blue"]).data[0])
-
-fig.update_layout(title='North America', title_x=0.5)
-fig.show()
+    fig.update_layout(title='North America', title_x=0.5)
+    fig.show()
--- ui/prediction.py
+++ ui/prediction.py
@@ -4,6 +4,8 @@ import time
 import datetime
 from datetime import datetime
 import math
+import csv
+import map
 
 df = pd.read_csv('boats2.csv')
 
@@ -36,28 +38,35 @@ for i in range(len(df['MMSI'].unique())-302):
     parsedPreTime=pd.to_datetime(first.iloc[i]['BaseDateTime'])
     parsedPostTime=pd.to_datetime(second.iloc[i]['BaseDateTime'])
     timeDiff=parsedPostTime-parsedPreTime
-    print("Time difference: ", timeDiff.total_seconds(), " seconds") 
+    print("Time difference: ", timeDiff.total_seconds(), " seconds")
     
     # Uselss code (angiveligt)    
     # COGrad = 360 * math.pi/180
     # print("COGrad :", COGrad)
-    
+
     # directX = math.cos(COGrad)
     # directY = math.sin(COGrad)
     
     timeDiffFloat = timeDiff.total_seconds()
     
-    predLocX = first.iloc[i]['LAT'] + (first.iloc[i]['SOG']*(timeDiffFloat/60)/60)*first.iloc[i]['COG']/1500
-    predLocY = first.iloc[i]['LON'] + (first.iloc[i]['SOG']*(timeDiffFloat/60)/60)*first.iloc[i]['COG']/1500
-    
-    predLocX2 = first.iloc[i]['LAT'] + (first.iloc[i]['SOG']*(timeDiffFloat/60)/60)*second.iloc[i]['Heading']/1000
-    predLocY2 = first.iloc[i]['LON'] + (first.iloc[i]['SOG']*(timeDiffFloat/60)/60)*second.iloc[i]['Heading']/1000
+    predLocX = first.iloc[i]['LAT'] + (first.iloc[i]['SOG']*(timeDiffFloat/60)/60)*first.iloc[i]['COG']/1000
+    predLocY = first.iloc[i]['LON'] + (first.iloc[i]['SOG']*(timeDiffFloat/60)/60)*first.iloc[i]['COG']/1000
     
     print('pred LAT',predLocX)
     print('pred LON',predLocY)
     
-    print('pred LAT2',predLocX2)
-    print('pred LON2',predLocY2)
+    array = []
+    array.append(first.iloc[i]['MMSI'])
+    array.append(predLocX)
+    array.append(predLocY)
+    
+    print("FUTURE LOCATION: \n")
+    print("Boat: ", array[0])
 
-    # print("Direct x,y  ", directX,directY)
-    # print("Prediced x,y  ", predLocX,predLocY)
+with open('predictions.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    field = ["MMSI", "LAT", "LON"]
+    writer.writerow(field)
+    writer.writerow(array)
+
+map.plot()
