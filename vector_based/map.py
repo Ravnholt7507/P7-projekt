99a5646d35aec5e5e6f52baef4b9cf976eea87f6
Run396 <Run396@users.noreply.github.com>
2023-10-12 10:30:51 +0200

    Update map.py

--- vector_based/map.py
+++ vector_based/map.py
@@ -1,11 +1,12 @@
 import plotly.express as px
 import pandas as pd
 
+
 def plot():
     df = pd.read_csv('boats.csv')
 
-    fig = px.scatter_geo(df, lat='LAT', lon='LON', hover_data=['BaseDateTime','SOG','COG','Heading'],
-                        hover_name='MMSI', color_discrete_sequence=['red'])
+    fig = px.scatter_geo(df, lat='LAT', lon='LON', hover_data=['BaseDateTime', 'SOG', 'COG', 'Heading'],
+                         hover_name='MMSI', color_discrete_sequence=['red'])
     fig.update_geos(
         scope='north america',
         resolution=110,  # Set to 50 or 110
@@ -15,7 +16,8 @@ def plot():
 
     df2 = pd.read_csv('predictions.csv')
 
-    fig.add_trace(px.scatter_geo(df2,lat='LAT', lon='LON',hover_data=['BaseDateTime','SOG'], hover_name='MMSI', color_discrete_sequence=['green']).data[0])
+    fig.add_trace(px.scatter_geo(df2, lat='LAT', lon='LON', hover_data=[
+                  'BaseDateTime', 'SOG'], hover_name='MMSI', color_discrete_sequence=['green']).data[0])
 
     fig.update_layout(title='North America', title_x=0.5)
     fig.show()
