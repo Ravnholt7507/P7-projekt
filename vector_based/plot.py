6e4ace70a16b0780260102e28258841c421d9de8
LuciferLied <Benjamin_porsmose@hotmail.com>
2023-10-12 10:47:40 +0200

    Plot update

--- vector_based/plot.py
+++ vector_based/plot.py
@@ -6,14 +6,11 @@ def plot():
     df = pd.read_csv("boats.csv")
     prediction = pd.read_csv("predictions.csv")
 
-    # print(df2)
-
     MMSI = prediction['MMSI'].unique()
     Actual_ship = df[df['MMSI'] == MMSI[0]]
 
     act_lats = Actual_ship['LAT'].tolist()
     act_lons = Actual_ship['LON'].tolist()
-
     pred_lats = prediction['LAT'].tolist()
     pred_lons = prediction['LON'].tolist()
 
@@ -33,6 +30,33 @@ def plot():
                   pred_lats[x], color='blue', width=0.00001, head_width=0.00005, length_includes_head=True)
     # plt.show()
     plt.savefig('predPlot.png')
+    plt.close()
 
+def actualToPred():
+    df = pd.read_csv("boats.csv")
+    prediction = pd.read_csv("predictions.csv")
 
-plot()
+    MMSI = prediction['MMSI'].unique()
+    Actual_ship = df[df['MMSI'] == MMSI[0]]
+
+    act_lats = Actual_ship['LAT'].tolist()
+    act_lons = Actual_ship['LON'].tolist()
+    pred_lats = prediction['LAT'].tolist()
+    pred_lons = prediction['LON'].tolist()
+
+    plt.figure()
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
+    plt.plot(act_lons, act_lats, 'red', marker='.', label='Actual', linestyle='None')
+    plt.plot(pred_lons, pred_lats, 'blue', marker='.', label='Predicted', linestyle='None')
+    plt.legend(loc='upper right')
+    
+    for x in range(len(act_lats)-1):
+        plt.arrow(act_lons[x], act_lats[x], pred_lons[x]-act_lons[x], pred_lats[x] - act_lats[x], color='green', width=0.00001, head_width=0.00005, length_includes_head=True)
+        
+    #plt.show()
+    plt.savefig('predPlotArrows.png')
+    plt.close()
+    
+#plot()
+#actualToPred()
