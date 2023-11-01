a21878fcfd2bf15a089ada180e90f049a5dfb19a
snotskovl <enorvi20@student.aau.dk>
2023-11-01 14:06:41 +0100

    Hjælp

---
+++ vector_based/fuckcleansedata.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def cleanse():
+     pd.set_option('display.max_columns', 20)
+
+     n = 150000
+
+     df = pd.read_csv('data/AIS_2023_01_01.csv',nrows=n)
+
+     df = df.sort_values(by=['MMSI', 'BaseDateTime'])
+
+     df = df[df.SOG > 5]
+
+     df = df[(df.LAT > 23) & (df.LAT < 24) & (df.LON > -82) & (df.LON < -80)]
+
+     #df = df.drop_duplicates(subset='MMSI', keep='First')
+
+     df = df.groupby('MMSI').tail(2)
+
+     df.to_csv('data/Collision_boats.csv', index=False)
