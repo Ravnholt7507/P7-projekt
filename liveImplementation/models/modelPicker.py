ab6ba30b9de4643c513ab08c007968000bf05030
Nathan Reece <nreece20@student.aau.dk>
2023-12-02 22:35:49 +0100

    Finished implementation of pure vector based prediction

--- liveImplementation/models/modelPicker.py
+++ liveImplementation/models/modelPicker.py
@@ -2,8 +2,14 @@ import models.traditionPredModels as traditionalModels
 import models.AIPredModels as AIModels
 
 def modelPicker(lastKnownLocations):
-        if lastKnownLocations[-1]['SOG'] * 1.852 < 0.3:
+        """ if lastKnownLocations[-1]['SOG'] * 1.852 < 0.3:
                 return traditionalModels.pointBasedModel(lastKnownLocations[-1])
+        elif lastKnownLocations[-1]['SOG'] * 1.852 > 0.3:
+                return traditionalModels.COGBasedModel(lastKnownLocations[-1])
+        else:
+                return traditionalModels.vectorBasedModel(lastKnownLocations) """
+        if len(lastKnownLocations)>1:
+                return traditionalModels.vectorBasedModel(lastKnownLocations)
         else:
                 return traditionalModels.COGBasedModel(lastKnownLocations[-1])
         #If "not first run" + other conditions
--- liveImplementation/models/traditionPredModels.py
+++ liveImplementation/models/traditionPredModels.py
@@ -2,17 +2,21 @@ from math import radians, sin, cos, sqrt, atan2
 import haversine as hv
 from haversine import haversine
 from geopy.distance import distance
+from geopy.point import Point
 import globalVariables as globals
+from geographiclib.geodesic import Geodesic
 
 class pointBasedModel:
 
     def __init__(self, currentLocation) -> None:
         self.thresholdCoordinates = (currentLocation['LAT'], currentLocation['LON'])
     
+    #REPEATING
     #Defines the behaviour when printing an instance of the class
     def __str__(self):
         return("pointBasedModel")
 
+    #REPEATING
     #Determines the threshold from which the area that confines the boat is determined
     def determineThreshold(self, lastKnownLocations):
         currentLocation = lastKnownLocations[-1]
@@ -20,6 +24,7 @@ class pointBasedModel:
         thresholdCoordinates = (currentLocation['LAT'], currentLocation['LON'])
         return thresholdCoordinates, radiusThreshold
     
+    #REPEATING
     #Is responsible for the contious estimation of the boat location
     def runPredictionAlgorithm(self, predictedCoordinates):
         return self.thresholdCoordinates
@@ -38,11 +43,11 @@ class COGBasedModel:
         #print("COGBasedModel: Determining threshold")
         initialCoordinates = (currentLocation['LAT'],currentLocation['LON'])
         self.COG = currentLocation['COG']
-        radiusThreshold = 0.5
+        self.radiusThreshold = 0.5
         self.speed = currentLocation['SOG'] * 1.852
-        thresholdCoordinates = distance(kilometers=radiusThreshold).destination(initialCoordinates, self.COG)
+        thresholdCoordinates = distance(kilometers=self.radiusThreshold).destination(initialCoordinates, self.COG)
         #print("COGBasedModel: Threshold determined as: ", thresholdCoordinates[0], thresholdCoordinates[1])
-        return thresholdCoordinates, radiusThreshold
+        return thresholdCoordinates, self.radiusThreshold
 
     def runPredictionAlgorithm(self, predictedCoordinates):
         #print("COGBasedModel: Running predictionAlgorithm")
@@ -57,10 +62,17 @@ class vectorBasedModel:
         return("vectorBasedModel")
 
     def determineThreshold(self, lastKnownLocations):
-        radiusThreshold = 0.5
-        #previousLocation = 
-        return radiusThreshold
+        self.radiusThreshold = 0.5
+        previousCoordinates = lastKnownLocations[-2]['LAT'], lastKnownLocations[-2]['LON']
+        initialCoordinates = lastKnownLocations[-1]['LAT'], lastKnownLocations[-1]['LON']
+        self.speed = lastKnownLocations[-1]['SOG'] * 1.852
+        #Calculates COG based on two last positions
+        COGresult = Geodesic.WGS84.Inverse(previousCoordinates[0], previousCoordinates[1], initialCoordinates[0], initialCoordinates[1])
+        self.COG = COGresult['azi1']
+        thresholdCoordinates = distance(kilometers=self.radiusThreshold).destination(initialCoordinates, self.COG)
+        return thresholdCoordinates, self.radiusThreshold
     
-    def runPredictionAlgorithm(self):
-        return
+    def runPredictionAlgorithm(self, predictedCoordinates):
+        distanceTravelled = self.speed * (globals.timeIntervals / 3600)
+        return distance(kilometers=distanceTravelled).destination(predictedCoordinates, self.COG)
     
