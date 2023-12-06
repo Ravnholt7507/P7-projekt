43c6f87d402c0d6cb8f97af56583aa1c80153219
Nathan Reece <nreece20@student.aau.dk>
2023-12-06 14:14:36 +0100

    test

--- liveImplementation/globalVariables.py
+++ liveImplementation/globalVariables.py
@@ -5,32 +5,3 @@ timeIntervals = 10
 readLimit = 50000
 output_CSV = pd.DataFrame()
 tts = 0
-
-
-class TextToSpeech:
-    def __init__(self, voice_id=None, speed=100):
-        self.voice_id = voice_id
-        self.speed = speed
-        self.engine = pyttsx3.init()
-        self.voices = self.engine.getProperty('voices')
-        self.set_voice(self.voice_id)
-        self.set_speed(self.speed)
-        self.speech_enabled = True
-
-    def toggle_speech(self, enable=True):
-        self.speech_enabled = enable
-
-    def set_voice(self, voice_id):
-        if voice_id is not None and 0 <= voice_id < len(self.voices):
-            self.engine.setProperty('voice', self.voices[voice_id].id)
-
-    def set_speed(self, speed):
-        self.engine.setProperty('rate', speed)
-
-    def speak(self, text, *variables):
-        if self.speech_enabled:
-            variable_strings = [str(variable) for variable in variables]
-            speech_text = text.format(*variable_strings)
-            print(speech_text)
-            self.engine.say(speech_text)
-            self.engine.runAndWait()
--- liveImplementation/main.py
+++ liveImplementation/main.py
@@ -10,20 +10,12 @@ import numpy as np
 import time
 from tqdm import tqdm
 
-
-# gv.tts = gv.TextToSpeech(voice_id=1, speed=170)
-# gv.tts.toggle_speech(enable=True)
-# gv.tts.speak("\nNarration is enabled to enhance program understanding. To run program normally, please pass False as boolean value to the toggle speech function found in main.\n\n")
-# gv.tts.speak("Initiating program.\n\n")
-
 start_time = time.time()
 
 #Initialize working data and output data
 
-# gv.tts.speak("Interpolating data. Please wait.")
 interpolated_data = dataHandler.interpolater()
 interpolated_data.to_csv('../data/interpolated_data.csv', index=False)
-# gv.tts.speak("Interpolation process completed.\n\n")
 
 interpolated_data = pd.read_csv('../data/interpolated_data.csv')
 scaler = dataHandler.Fit_Scaler_To_Data(interpolated_data)
@@ -41,18 +33,14 @@ output_CSV['currentModel'] = None
 
 interpolated_data = interpolated_data.groupby('MMSI')
 
-# gv.tts.speak("Executing simulation.")
 simulationOutput = np.empty((0, 7))
 i = 1
 
 length = len(interpolated_data)
 # Run simulation for each unique boat
 for name, group in tqdm(interpolated_data, desc="Running simulation"):
-    # gv.tts.speak("Creating instance of shore object.\n\n")
     shore_instance = shore.shoreEntity(group.iloc[0])
-    # gv.tts.speak("Creating instance of boat object.\n\n")
     boat_instance = boat.boatEntity(group.iloc[0])
-    # gv.tts.speak("   Simulating group {} of {} total groups\n", i, length)
     simulation_instance = simulationClass.simulation(group, shore_instance, boat_instance)
     simulationOutput = np.vstack((simulationOutput, simulation_instance.run_simulation()))
     
