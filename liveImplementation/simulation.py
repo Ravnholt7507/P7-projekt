f5a77e6e779cb6965b5f87c01962ff257b8f22d2
Run396 <Run396@users.noreply.github.com>
2023-12-06 12:01:41 +0100

    tioiteist

--- liveImplementation/simulation.py
+++ liveImplementation/simulation.py
@@ -13,16 +13,16 @@ class simulation:
     def run_simulation(self):
         #While-loop simulates time progression -> run simulation for each instance of the boat
         while self.current_index+1 <= len(self.group):
-            # gv.tts.speak("   Simulating instance {} of {} total instances for current boat:\n", self.current_index+1, len(self.group))
-            # gv.tts.speak("   Simulation executing boat behaviour:\n")
+            gv.tts.speak("   Simulating instance {} of {} total instances for current boat:\n", self.current_index+1, len(self.group))
+            gv.tts.speak("   Simulation executing boat behaviour:\n")
             instanceOutput = self.boatEntity.boatBehaviour(self.group.iloc[self.current_index], self.shoreEntity)
-            # gv.tts.speak("   Simulation executing shore behaviour:\n")
+            gv.tts.speak("   Simulation executing shore behaviour:\n")
             self.shoreEntity.shoreBehaviour()
             self.current_index = self.current_index+1
-            # gv.tts.speak("   Simulation stacking instance results for current run on the total result array.")
+            gv.tts.speak("   Simulation stacking instance results for current run on the total result array.")
             self.completeOutput = np.vstack((self.completeOutput, instanceOutput))
-            # gv.tts.speak("   Simulation instance complete. Incrementing time.\n\n")
+            gv.tts.speak("   Simulation instance complete. Incrementing time.\n\n")
 
-        # gv.tts.speak("Group simulation complete. Returning intermediary output for group.")
+        gv.tts.speak("Group simulation complete. Returning intermediary output for group.")
         return self.completeOutput
             
