6818a75fa2459a0a62edcb043df7e04faf6f360d
Run396 <Run396@users.noreply.github.com>
2023-12-08 12:45:17 +0100

    no dupes wohooo

--- vector_based/collision.py
+++ vector_based/collision.py
@@ -178,11 +178,13 @@ def find_vector_colission(ship_data, num_clusters):
                         # Calculate time difference between the two ships
                         time_diff = abs(cluster_data['BaseDateTime'][x] - cluster_data['BaseDateTime'][y])
 
-                                                
                         # Save MMSI, lat, lon and collision points between the two ships to a csv file
                         with open('data/vector_colissions.csv', 'a') as fp:
                             fp.write(f"{cluster_data['MMSI'].iloc[x]},{pos1},{vec1},{cluster_data['BaseDateTime'][x]},{cluster_data['MMSI'].iloc[y]},{pos2},{vec2},{cluster_data['BaseDateTime'][y]},{intersection},{time_diff}\n")
-                            # add time to csv
-                    
-
+    
+    # remove duplicate rows from vector_colissions.csv
+    df = pd.read_csv('data/vector_colissions.csv')
+    df = df.drop_duplicates()
+    df.to_csv('data/vector_colissions.csv', index=False)
+    
     print(collisions)
