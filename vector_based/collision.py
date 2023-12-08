c35dc4f0c7594dd533c8934d347e2a88c77d6d79
Run396 <Run396@users.noreply.github.com>
2023-12-08 11:41:28 +0100

    time

--- vector_based/collision.py
+++ vector_based/collision.py
@@ -181,7 +181,9 @@ def find_vector_colission(ship_data, num_clusters):
                                                 
                         # Save MMSI, lat, lon and collision points between the two ships to a csv file
                         with open('data/vector_colissions.csv', 'a') as fp:
-                            fp.write(f"{cluster_data['MMSI'].iloc[x]},{pos1},{vec1},{cluster_data['MMSI'].iloc[y]},{pos2},{vec1},{intersection},{time_diff}\n")
+                            fp.write(f"{cluster_data['MMSI'].iloc[x]},{pos1},{vec1},{cluster_data['BaseDateTime'][x]},{cluster_data['MMSI'].iloc[y]},{pos2},{vec1},{cluster_data['BaseDateTime'][y]},{intersection},{time_diff}\n")
+                            # add time to csv
+                            fp.write
                     
 
     print(collisions)
