66ad1143e169c428a1366218d27d1c28bdcac125
Nathan Reece <nreece20@student.aau.dk>
2023-12-10 13:47:45 +0100

    time intevals to standard

--- liveImplementation/globalVariables.py
+++ liveImplementation/globalVariables.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-timeIntervals = 120
+timeIntervals = 10
 readLimit = 50000
 output_CSV = pd.DataFrame()
 scaler = MinMaxScaler()
