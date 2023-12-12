24af594ffdbadae1d462fa2eea689896f68d80e4
Run396 <Run396@users.noreply.github.com>
2023-12-12 13:15:39 +0100

    leng is perfect og kan bruges på hele datasettet

--- liveImplementation/lengh.py
+++ liveImplementation/lengh.py
@@ -1,72 +1,48 @@
 import pandas as pd
-from geopy.distance import geodesic
+from tqdm import tqdm
 
 df = pd.read_csv("../data/AIS_2023_01_01.csv")
-df = df[(df['LAT'] > 23) & (df['LAT'] < 24) & (df['LON'] > -82) & (df['LON'] < -80)]
+
+# Drop rows where heading == 511.0
+df = df[df['Heading'] != 511.0]
+
+# Drop whole MMSI where sog == 102.3
+df = df[df['SOG'] != 102.3]
+
 df = df.sort_values(by=['MMSI', 'BaseDateTime'], ascending=True)
 
-# Data is on the form:
-# MMSI, BaseDateTime, LAT, LON, SOG, COG, Heading, VesselName, IMO, CallSign, VesselType, Status, Length, Width, Draft, Cargo, TransceiverClass
-# Calculate distance between two points of each row per mmsi
-for mmsi in df['MMSI'].unique():
-    ship = df[df['MMSI'] == mmsi]
-    length = len(ship)
-    for i in range(length - 1):
-        lat1 = ship.iloc[i]['LAT']
-        lon1 = ship.iloc[i]['LON']
-        lat2 = ship.iloc[i + 1]['LAT']
-        lon2 = ship.iloc[i + 1]['LON']
-        dis = geodesic((lat1, lon1), (lat2, lon2)).km
-        df.loc[ship.index[i], 'distance'] = dis
-    
+# remove duplicates
+df = df.drop_duplicates(subset=['MMSI', 'BaseDateTime'], keep='first')
+
+# Calculate change in latitude and longitude for each row per MMSI
+df['LAT_change'] = df.groupby('MMSI')['LAT'].diff()
+df['LON_change'] = df.groupby('MMSI')['LON'].diff()
+
 # Calculate time between two points of each row per mmsi
-for mmsi in df['MMSI'].unique():
-    ship = df[df['MMSI'] == mmsi]
-    length = len(ship)
-    for i in range(length - 1):
-        time1 = ship.iloc[i]['BaseDateTime']
-        time2 = ship.iloc[i + 1]['BaseDateTime']
-        time_diff = (pd.to_datetime(time2) - pd.to_datetime(time1)).total_seconds()
-        df.loc[ship.index[i], 'time'] = time_diff
+df['BaseDateTime_shifted'] = df.groupby('MMSI')['BaseDateTime'].shift()
+df['time'] = (pd.to_datetime(df['BaseDateTime']) - pd.to_datetime(df['BaseDateTime_shifted'])).dt.total_seconds()
 
 # Calculate speed between two points of each row per mmsi
-for mmsi in df['MMSI'].unique():
-    ship = df[df['MMSI'] == mmsi]
-    length = len(ship)
-    for i in range(length - 1):
-        dis = ship.iloc[i]['distance']
-        time = ship.iloc[i]['time']
-        speed = dis / time
-        df.loc[ship.index[i], 'speed'] = speed
-
-# Drop MMSI where speed is not 0 (ship is not stationary) and SOG is 0 (ship is not moving)
+df['speed'] = df['LAT_change'] / df['time']
 
 # Calculate difference between SOG and speed
 df['diff'] = df['SOG'] - df['speed']
 
-# Drop MMSI where distance is above 1 and time is below 10
-df = df.drop(df[(df['distance'] > 5) & (df['time'] < 5)].index)
+# Drop rows based on conditions
+# 0.014 degrees per second corresponds to ~50 knots
+# Drop rows where latitude change is greater than 0.014 degrees per second and time is less than 5 seconds
+# Identify MMSI values that have any failing records
+failing_mmsi = df[(df['LAT_change'].abs() > 0.014) & (df['time'] < 5) |
+                  (df['LON_change'].abs() > 0.014) & (df['time'] < 5) |
+                  (df['LAT_change'].abs() > 0.014) & (df['SOG'] < 0.3) |
+                  (df['LON_change'].abs() > 0.014) & (df['SOG'] < 0.3) |
+                  (df['LAT_change'] == 0) |
+                  (df['LON_change'] == 0) |
+                  (df['diff'].abs() > 10)]['MMSI'].unique()
 
-# Drop MMSI where distance is above 1 and SOG is below 1
-df = df.drop(df[(df['distance'] > 5) & (df['SOG'] < 0.3)].index)
-
-# Drop MMSI where distance == 0
-df = df.drop(df[df['distance'] == 0].index)
-
-# Drop rows where heading == 511.0
-df = df.drop(df[df['Heading'] == 511.0].index)
-
-# Drop whole MMSI where sog == 102.3
-df = df.drop(df[df['SOG'] == 102.3].index)
-
-# Drop whole MMSI where abs diff > 10
-df = df.drop(df[abs(df['diff']) > 10].index)
-
-# remove distance,time,speed,diff
-df = df.drop(columns=['distance', 'time', 'speed', 'diff'])
+# Drop all records with failing MMSI values
+df = df[~df['MMSI'].isin(failing_mmsi)]
+# remove extra columns
+df = df.drop(columns=['LAT_change', 'LON_change', 'BaseDateTime_shifted', 'time', 'speed', 'diff'])
 
 df.to_csv('../data/filtered.csv', index=False)
-
-# output_df = pd.read_csv('../data/output.csv')
-# output_df.sort_values(by=['MMSI','SOG'], inplace=True, ascending=False)
-# output_df.to_csv('../data/output.csv', index=False)
