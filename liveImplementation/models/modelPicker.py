b376095cf0478646abf58827ab5d5b39992bf945
Nathan Reece <nreece20@student.aau.dk>
2023-12-15 16:00:02 +0100

    Update to modelPicker

--- liveImplementation/models/modelPicker.py
+++ liveImplementation/models/modelPicker.py
@@ -1,44 +1,65 @@
 import models.traditionPredModels as traditionalModels
+import random
+import math
+import numpy as np
 
 def modelPicker(lastKnownLocations):
-        '''
-        Make a new model picker that uses the last known location to determine which model to use
-        The models are:
-            - Point based
-            - COG based
-            - Vector based
-            - AI based
-            - If Heading = 511, go to next case
-        '''
-        '''
-        if lastKnownLocations[-1]['SOG'] * 1.852 < 0.1 or (lastKnownLocations[-1]['COG'] == None and len(lastKnownLocations) < 2 and lastKnownLocations[-1]['SOG'] * 1.852 < 0.1):
-            if lastKnownLocations[-1]['SOG']>200:
-                print(lastKnownLocations[-1]['SOG'])
-            return traditionalModels.pointBasedModel(lastKnownLocations[-1])        
-        else:
-            if lastKnownLocations[-1]['SOG']>200:
-                print(lastKnownLocations[-1]['SOG'])            
-            return traditionalModels.COGBasedModel(lastKnownLocations[-1])
-        elif len(lastKnownLocations) >= 2 and lastKnownLocations[-1]['SOG'] * 1.852 > 0.3:
-            return traditionalModels.vectorBasedModel(lastKnownLocations)
-        
-        elif len(lastKnownLocations) < 2 and lastKnownLocations[-1]['COG'] == None and lastKnownLocations[-1]['SOG'] * 1.852 > 0.3:
-             return traditionalModels.HeadingBasedModel(lastKnownLocations)
-        
-        elif len(lastKnownLocations) >= 8 and average_COG(lastKnownLocations) > 5:
-            return traditionalModels.AIBasedModel(lastKnownLocations)
-        
-        else:
-            #print("Default Case")
-            return traditionalModels.pointBasedModel(lastKnownLocations[-1]) 
+    '''
+    sMake a new model picker that uses the last known location to determine which model to use
+    The models are:
+        - Point based
+        - COG based
+        - Vector based
+        - AI based
+        - If Heading = 511, go to next case
+    '''
+    rateOfTurn = calcRateOfTurn(lastKnownLocations)
+    print("rateOfTurn", rateOfTurn)
 
-        '''
-		
-        if len(lastKnownLocations) == 10 and lastKnownLocations[-1]['SOG'] * 1.852 > 3:
-            return traditionalModels.AIBasedModel(lastKnownLocations)  
-        else:
-            return traditionalModels.pointBasedModel(lastKnownLocations[-1])
+    if (lastKnownLocations[-1]['SOG']<0.3):
+        return traditionalModels.pointBasedModel(lastKnownLocations[-1])
+    
+    if (rateOfTurn >= 7 and len(lastKnownLocations) == 10):
+          return traditionalModels.AIBasedModel(lastKnownLocations)
+    
+    if (lastKnownLocations[-1]['COG'] != None):
+        return traditionalModels.COGBasedModel(lastKnownLocations[-1])
+    
+    if (lastKnownLocations[-1]['Heading'] != None): 
+        return traditionalModels.HeadingBasedModel(lastKnownLocations[-1])
+    
+    if (lastKnownLocations[-1]['COG'] != None and lastKnownLocations[-1]['Heading'] != None and len(lastKnownLocations)>1):
+          return traditionalModels.vectorBasedModel(lastKnownLocations)
+    
+    else:
+        return traditionalModels.pointBasedModel(lastKnownLocations[-1])
+    
 
+def calcRateOfTurn(queue):
+    """ if queue[-1]['COG'] != None:
+        return
+    elif queue[-1]['Heading'] != None:
+        return """
+    if len(queue) > 2:
+
+        # Convert deque to a NumPy array and extract 'LAT' and 'LON'
+        locations = np.array([(entry['LAT'], entry['LON']) for entry in queue])
+
+        # Take the last three locations
+        last_three_locations = locations[-3:] 
+        vectors = np.diff(last_three_locations, axis=0)
+
+        #Compute angles
+        angles = np.arctan2(vectors[:, 1], vectors[:, 0])
+        angle_diff = np.diff(angles)[-1]
+        angle_diff_degrees = np.degrees(angle_diff)
+
+        return angle_diff_degrees
+    
+    else:
+          return 0
+    
+      
 
 def average_COG(dataframe):
 	total = 0
